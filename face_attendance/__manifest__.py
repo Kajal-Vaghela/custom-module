4daa441a8c92dfc9f977c3548e460e1436223386
Cravit-KajalVaghela <kajal.vaghela@cravit.in>
2025-07-15 16:41:47 +0530

    Add module

---
+++ face_attendance/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Face Attendance",
+    "summary": """Face Attendance""",
+    "description": """Face Attendance.""",
+    "author": "Kajal Vaghela",
+    "category": "HR/Attendance",
+    "version": "17.0",
+    "license": "AGPL-3",
+    "depends": ["base", "web", "hr_attendance"],
+    "assets": {
+        "web.assets_backend": [
+            "face_attendance/static/src/attendance_menu/**/*",
+            "face_attendance/static/src/lib/face-api.min.js",
+        ],
+    },
+    "data": [
+        "views/hr_attendance_view.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": True,
+    "currency": "EUR",
+    "price": "49.0",
+}
